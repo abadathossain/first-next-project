3ff1e980b86d872d01d81a222f968a335c6ccb6c
Abadat Hossain <abadat26@gmail.com>
2025-02-04 22:04:40 +0600

    common layout with image

--- src/app/(withCommonLayout)/page.js
+++ src/app/(withCommonLayout)/page.js
@@ -1,7 +1,24 @@
+import Image from "next/image";
+import Img from "../../assets/next.jpg";
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-4xl text-center text-blue-600">Home Page</h1>
+      <h1 className="text-4xl text-center text-blue-600">Home local Page</h1>
+      <Image
+        src={Img}
+        alt="logo"
+        width={200}
+        height={200}
+        className="mx-auto"
+      />
+      <h1 className="text-4xl text-center text-blue-600">Home URL Page</h1>
+      <Image
+        src="https://miro.medium.com/v2/resize:fit:1400/1*BQZAbczBfLYtPp-6HmN0ZQ.jpeg"
+        alt="logo"
+        width={200}
+        height={200}
+        className="mx-auto"
+      />
     </div>
   );
 };
